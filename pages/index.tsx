b67bbf06b89f6d863bfdf18222ca41c8396fca82
sunaj <sjinsta71@gmail.com>
2025-04-09 20:43:20 +0530

    deploychanges

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,17 +4,16 @@ import MenuTabs from "@/components/MenuTabs";
 
 export default function Home() {
   return (
-    <div>
+    <>
       <Header />
-      <main style={{ padding: "30px 16px", textAlign: "center" }}>
-        <h1 style={{ fontSize: "42px", marginBottom: "8px" }}>MENU</h1>
-        <p>
-  Please take a look at our menu featuring food, drinks, and brunch. If you&apos;d like to place an order, use the &quot;Order Online&quot; button located below the menu.
-</p>
-
+      <main style={{ padding: "30px", textAlign: "center" }}>
+        <h1>Menu</h1>
+        <p style={{ maxWidth: "600px", margin: "10px auto" }}>
+          Please take a look at our menu featuring food, drinks, and brunch. If you&apos;d like to place an order, use the &quot;Order Online&quot; button located below the menu.
+        </p>
         <MenuTabs />
       </main>
       <Footer />
-    </div>
+    </>
   );
 }
